98c3495951af4293fefc743f46502606c5c048f5
Helen Peng <140556371+Hel-Pen@users.noreply.github.com>
2023-08-04 17:42:12 -0500

    Update Choupin.java

--- Choupin/src/main/java/org/Choupin.java
+++ Choupin/src/main/java/org/Choupin.java
@@ -1,10 +1,11 @@
-import net.dv8tion.jda.api.JDA;
+
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Choupin {
     public static void main(String[] args) {
-        final String TOKEN = "MTEzNDIxNTA5NTU4NTYwNzY4MA.GDxCJN.i7G1SWEOkc8_5tZuzzzY-XMUVb8q99QYxdfpwg";
+        final String TOKEN = "MTEzNDIxNTA5NTU4NTYwNzY4MA.GVf-M4.-YPw8nRp3AKBG74iyR2x0tfA9MDU7tEVz9xYao";
         JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
-        JDA jda = jdaBuilder.build();
+        jdaBuilder.build();
     }
 }
+
