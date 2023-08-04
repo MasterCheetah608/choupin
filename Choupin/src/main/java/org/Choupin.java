88b9f069a3b252b15f5a4ceefb7a96ce2710ba23
JH027 <141120288+JH027@users.noreply.github.com>
2023-08-04 15:56:28 -0700

    Update Choupin.java

--- Choupin/src/main/java/org/Choupin.java
+++ Choupin/src/main/java/org/Choupin.java
@@ -1,7 +1,7 @@
 
 import net.dv8tion.jda.api.JDABuilder;
 
-public class Choupin {
+public class Choupin throws LoginException{
     public static void main(String[] args) {
         final String TOKEN = "MTEzNDIxNTA5NTU4NTYwNzY4MA.GVf-M4.-YPw8nRp3AKBG74iyR2x0tfA9MDU7tEVz9xYao";
         JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
